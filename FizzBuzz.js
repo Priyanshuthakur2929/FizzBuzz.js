b15f1a3b243253aca53401b9c7448d738fbdb080
Your Name <your-email@example.com>
2026-07-08 12:14:56 +0530

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,14 @@
+for(let i =1; i<=20; i++){
+    if(i%3===0 && i%5===0){
+        console.log("FizzBuzz");
+    }
+    else if(i%3===0){
+        console.log("Fizzz");
+    }
+    else if(i%5===0){
+        console.log("Buzzzz");
+    }
+    else{
+        console.log(i);
+    }
+}
